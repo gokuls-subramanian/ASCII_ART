0f4db4f8339a1d3745e1ccccf8c8591e4a2ae7dc
GOKUL SUBRAMANIAN <96585296+gokuls-subramanian@users.noreply.github.com>
2025-04-27 12:37:31 +0530

    Update websitedeploy.py

--- websitedeploy.py
+++ websitedeploy.py
@@ -11,11 +11,13 @@ def load_ascii_frames(file_path):
 
 @app.route('/')
 def home():
-    return """Welcome to ASCII Live Server! Try following command in your console.
+
+    print("""Welcome to ASCII Live Server! Try following command in your console.
             1-curl https://ascii-art-78y3.onrender.com/sigmamale \n
             2-curl https://ascii-art-78y3.onrender.com/Gigachad  \n
             3-curl https://ascii-art-78y3.onrender.com/Trollface \n
-            4-curl https://ascii-art-78y3.onrender.com/Dancing"""
+            4-curl https://ascii-art-78y3.onrender.com/Dancing""")
+    return ""
 
 @app.route('/<name>')
 def stream_ascii(name):
